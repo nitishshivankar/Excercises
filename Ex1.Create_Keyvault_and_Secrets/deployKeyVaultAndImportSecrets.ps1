d4fdc93f911fa6da425233b47373ecd1fe52b4ed
Nitish Shivankar <nitish.shivankar@globant.com>
2020-02-04 18:22:01 +0530

    Excercise 1

---
+++ Ex1.Create_Keyvault_and_Secrets/deployKeyVaultAndImportSecrets.ps1
@@ -0,0 +1,6 @@
+﻿az login
+$resourceGroupName = Read-Host "Enter resource group name : "
+$currentFolderPath = Get-Location
+$templatePath = $currentFolderPath.Path + "/Template.json"
+az group create --name $resourceGroupName --location "West India"
+az group deployment create --name keyVaultDeployment --resource-group $resourceGroupName --template-file $templatePath
